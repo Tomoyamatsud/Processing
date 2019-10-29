47deed9df8e29e52b90993e0f789432b919a84bf
Tomoyamatsud <Tomoyamatsud@gmail.com>
2019-10-29 21:57:57 +0900

    exercise3.10の更新

--- Chapter3/exercise3_10/build/source/exercise3_10.java
+++ Chapter3/exercise3_10/build/source/exercise3_10.java
@@ -14,23 +14,36 @@ import java.io.IOException;
 
 public class exercise3_10 extends PApplet {
 
-Wave[] waves = new Wave[10];
+Wave[] waves1 = new Wave[10];
+Wave[] waves2 = new Wave[20];
 
 public void setup()
 {
   
 
-  for( int i = 0; i < waves.length; i++)
+  /* waves1\u306e\u521d\u671f\u5316 */
+  for( int i = 0; i < waves1.length; i++ )
   {
-    waves[i] = new Wave(random(width), random(height) );
+      waves1[i] = new Wave(random(width), random(height), i);
+  }
+
+  /* waves2\u306e\u521d\u671f\u5316 */
+  for( int i = 0; i < waves2.length; i++ )
+  {
+    waves2[i] = new Wave(random(width), random(height), i);
   }
 }
 
 public void draw()
 {
-  for( int i = 0; i < waves.length; i++ )
+  for( int i = 0; i < waves1.length; i++ )
   {
-    waves[i].display();
+    waves1[i].display();
+  }
+
+  for( int i = 0; i < waves2.length; i++ )
+  {
+    waves2[i].display();
   }
 }
 
@@ -38,11 +51,14 @@ class Wave
 {
   float xPos;
   float yPos;
+  float angle;
 
-  Wave( float initX, float initY )
+  /* \u30b3\u30f3\u30b9\u30c8\u30e9\u30af\u30bf */
+  Wave( float initX, float initY, float initAng )
   {
     xPos = initX;
     yPos = initY;
+    angle = initAng;
   }
 
   // \u30a4\u30f3\u30b9\u30bf\u30f3\u30b9\u3092\u753b\u9762\u306b\u8868\u793a\u3059\u308b
--- Chapter3/exercise3_10/exercise3_10.pde
+++ Chapter3/exercise3_10/exercise3_10.pde
@@ -1,20 +1,34 @@
-Wave[] waves = new Wave[10];
+Wave[] waves1 = new Wave[10];
+Wave[] waves2 = new Wave[20];
 
 void setup()
 {
   size(800,200);
 
-  for( int i = 0; i < waves.length; i++)
+  /* waves1の初期化 */
+  /* 配列ではなく、先頭の要素が決まれば、数珠繋ぎで表示する玉に変更する */
+  for( int i = 0; i < waves1.length; i++ )
   {
-    waves[i] = new Wave(random(width), random(height) );
+      waves1[i] = new Wave(random(width), random(height), i);
+  }
+
+  /* waves2の初期化 */
+  for( int i = 0; i < waves2.length; i++ )
+  {
+    waves2[i] = new Wave(random(width), random(height), i);
   }
 }
 
 void draw()
 {
-  for( int i = 0; i < waves.length; i++ )
+  for( int i = 0; i < waves1.length; i++ )
   {
-    waves[i].display();
+    waves1[i].display();
+  }
+
+  for( int i = 0; i < waves2.length; i++ )
+  {
+    waves2[i].display();
   }
 }
 
@@ -22,11 +36,14 @@ class Wave
 {
   float xPos;
   float yPos;
+  float angle;
 
-  Wave( float initX, float initY )
+  /* コンストラクタ */
+  Wave( float initX, float initY, float initAng )
   {
     xPos = initX;
     yPos = initY;
+    angle = initAng;
   }
 
   // インスタンスを画面に表示する
