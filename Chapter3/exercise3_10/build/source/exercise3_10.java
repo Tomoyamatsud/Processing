8e5b75010b218cdfdce20ae272021cb7cb11c819
Tomoyamatsud <Tomoyamatsud@gmail.com>
2019-11-12 21:56:09 +0900

    exercise3.10の更新

--- Chapter3/exercise3_10/build/source/exercise3_10.java
+++ Chapter3/exercise3_10/build/source/exercise3_10.java
@@ -14,40 +14,32 @@ import java.io.IOException;
 
 public class exercise3_10 extends PApplet {
 
-Wave[] waves1;
-Wave[] waves2;
+Wave waves1;
+Wave waves2;
 
-float addangle = 10;
-float addxPos = 24;
+float startangle = 0;
 
 public void setup()
 {
   
 
+  float initX_for_waves1 = map(random(width), 0, width, 30, width-50);
+  float initX_for_waves2 = map(random(width), 0, width, 30, width-50);
+
   /* waves1\u306e\u521d\u671f\u5316 */
-  for( int n = 0; n < waves1.length; n++)
-  {
-      waves1[n] = new Wave(n*addxPos, n*addangle);
-  }
+  waves1 = new Wave(initX_for_waves1, 10);
   /* waves2\u306e\u521d\u671f\u5316 */
-  for( int n = 0; n < waves2.length; n++)
-  {
-      waves2[n] = new Wave(n*addxPos, n*addngle);
-  }
+  waves2 = new Wave(initX_for_waves2, 20);
 }
 
 public void draw()
 {
   background(255);
 
-  for( int i = 0; i < waves1.length; n++)
-  {
-    waves1[i].display();
-  }
-  for( int i = 0; i < waves2.length; n++)
-  {
-    waves2[i].display();
-  }
+  float initangle = startangle;
+
+  waves1.display(initangle, 0.25f, height);
+  waves2.display(initangle, 0.74f, height/4);
 }
 
 class Wave
@@ -55,21 +47,29 @@ class Wave
   float xPos;
   float yPos;
   int size;
+  int length;
 
   /* \u30b3\u30f3\u30b9\u30c8\u30e9\u30af\u30bf */
-  Wave( float initX, float initAng )
+  Wave( float initX, int setlength )
   {
     xPos = initX;
-    yPos = height*sin(initAng);
+    yPos = height/2;
     size = 24;
+    length = setlength;
   }
 
   // \u30a4\u30f3\u30b9\u30bf\u30f3\u30b9\u3092\u753b\u9762\u306b\u8868\u793a\u3059\u308b
-  public void display()
+  public void display(float angle, float angleVel, float amplitude )
   {
     stroke(0);
     noFill();
-    ellipse(xPos,yPos,size,size);
+
+    for( int n=0; n < length; n++)
+    {
+      float y = map(sin(angle), -1, 1, 0, amplitude);
+      ellipse(xPos+size*n,y,size,size);
+      angle += angleVel;
+    }
   }
 }
   public void settings() {  size(800,200); }
--- Chapter3/exercise3_10/exercise3_10.pde
+++ Chapter3/exercise3_10/exercise3_10.pde
@@ -1,37 +1,29 @@
-Wave[] waves1;
-Wave[] waves2;
+Wave waves1;
+Wave waves2;
 
-float addangle = 10;
-float addxPos = 24;
+float startangle = 0;
 
 void setup()
 {
   size(800,200);
 
+  float initX_for_waves1 = map(random(width), 0, width, 30, width-50);
+  float initX_for_waves2 = map(random(width), 0, width, 30, width-50);
+
   /* waves1の初期化 */
-  for( int n = 0; n < waves1.length; n++)
-  {
-      waves1[n] = new Wave(n*addxPos, n*addangle);
-  }
+  waves1 = new Wave(initX_for_waves1, 10);
   /* waves2の初期化 */
-  for( int n = 0; n < waves2.length; n++)
-  {
-      waves2[n] = new Wave(n*addxPos, n*addngle);
-  }
+  waves2 = new Wave(initX_for_waves2, 20);
 }
 
 void draw()
 {
   background(255);
 
-  for( int i = 0; i < waves1.length; n++)
-  {
-    waves1[i].display();
-  }
-  for( int i = 0; i < waves2.length; n++)
-  {
-    waves2[i].display();
-  }
+  float initangle = startangle;
+
+  waves1.display(initangle, 0.25, height);
+  waves2.display(initangle, 0.74, height/4);
 }
 
 class Wave
@@ -39,20 +31,28 @@ class Wave
   float xPos;
   float yPos;
   int size;
+  int length;
 
   /* コンストラクタ */
-  Wave( float initX, float initAng )
+  Wave( float initX, int setlength )
   {
     xPos = initX;
-    yPos = height*sin(initAng);
+    yPos = height/2;
     size = 24;
+    length = setlength;
   }
 
   // インスタンスを画面に表示する
-  void display()
+  void display(float angle, float angleVel, float amplitude )
   {
     stroke(0);
     noFill();
-    ellipse(xPos,yPos,size,size);
+
+    for( int n=0; n < length; n++)
+    {
+      float y = map(sin(angle), -1, 1, 0, amplitude);
+      ellipse(xPos+size*n,y,size,size);
+      angle += angleVel;
+    }
   }
 }
